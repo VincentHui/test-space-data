7621fd9e34b4571dd7c69fb20eee545d409597d6
Tiana Robinson <trobinson1097@gmail.com>
2023-10-16 00:06:48 -0700

    Four Items displaying along with two non-functioning buttons. Little CSS.

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import axios from "axios";
 
 function App() {
+  const [items, itemsSetter] = useState([]); //setting state for items
+
   useEffect(() => {
     const url =
       "https://corsproxy.io/?" +
       encodeURIComponent(
         "https://eg05cvpjn2.execute-api.eu-west-2.amazonaws.com/stage/queryResults?teamId=test_tbb_char_1.3"
       );
+
+    // I am saving this spot for the buttons function 
+
 
     const get = async () => {
       const res = await axios.get(url, {
@@ -18,11 +23,32 @@ function App() {
         },
         // params: { teamId: "test_tbb_char_1.3" },
       });
-      console.log(res);
+      console.log(res.data.queryResult.Items);
+      itemsSetter(res.data.queryResult.Items)
     };
     get();
   }, []);
-  return <></>;
+
+
+  return (
+    <>
+      <button className="button leftButton"> Left Button </button>
+      <button className="button rightButton"> Right Button</button>
+      {items.map(
+        (obj: any) => {
+          return (
+            <div>
+              <div className="items" key={obj.Items}>
+                <div className="item UnixTimeAdded"> UnixTimeAdded: {obj?.UnixTimeAdded.N}</div>
+                <div className="item current1"> current1: {obj?.current1.N}</div>
+                <div className="item current2"> current2: {obj?.current2.N}</div>
+                <div className="item enabledwire4"> enabledwire4: {obj?.enabledwire4.N}</div>
+              </div>
+            </div>
+          )
+        })}
+    </>
+  );
 }
 
 export default App;
